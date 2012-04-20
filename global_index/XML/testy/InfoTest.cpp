aef257ca327b084e4fc6dcab33daa0e3c1c48212
mika <mika@mika-K52Je.(none)>
2012-04-20 17:37:19 +0200

    Test Info

--- global_index/XML/testy/InfoTest.cpp
+++ global_index/XML/testy/InfoTest.cpp
@@ -13,32 +13,60 @@ int main()
     //konstruktor domyślny
     Info in;
     string na="non";
+    cout<<"Kontruktor domyślny ->";
     if(in.getType()==na)
+       {
         ++ok;
+        cout<<"OK"<<endl;
+       }
     else
+    {
         ++failed;
+        cout<<"FAIL"<<endl;
+    }
     string nazwa="nowa";
     //konatruktor od string
+    cout<<"Konstruktor zależny od string ->";
     Info in2(nazwa);
     if(in2.getType()==nazwa)
+    {
         ++ok;
+        cout<<"OK"<<endl;
+       }
     else
+    {
         ++failed;
-
+        cout<<"FAIL"<<endl;
+    }
+    cout<<"Zmiana typu ->";
     //nadanie typu
     in.setType(na);
     if(in.getType()==na)
+   {
         ++ok;
+        cout<<"OK"<<endl;
+       }
     else
+    {
         ++failed;
+        cout<<"FAIL"<<endl;
+    }
     //kopia
+    cout<<"Kopiowanie do innego obiektu tej klasy ->";
     in.Copy(in2);
     if(in.getType()==in2.getType())
+      {
         ++ok;
+        cout<<"OK"<<endl;
+       }
     else
+    {
         ++failed;
-
+        cout<<"FAIL"<<endl;
+    }
+    cout<<endl;
     cout<<"Przeprowadzono "<<ok+failed<<" testów, gdzie:"<<endl<<ok<<" pomyślnie zakończonych"<<endl<<failed<<" zakończonych niepowodzeniem";
+    cout<<endl;
     return 0;
 }
 
