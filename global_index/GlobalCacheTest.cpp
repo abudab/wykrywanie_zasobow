847018bea598c617bb9b0fadc5a27e76064548b9
nshadov ! <nsh.adov.eq@gmail.com>
2012-05-06 13:26:16 +0200

    responce.cpp -> response.cpp

--- global_index/GlobalCacheTest.cpp
+++ global_index/GlobalCacheTest.cpp
@@ -21,6 +21,16 @@
 int main(){
   
   GlobalCache gc = GlobalCache();
+
+  std::string str("<Search name='request_001' id='12345'><Filters><Filter name='CPU-Frequency' />"
+	     "<Filter name='MEM-Count' /><Filter name='OS-Name' /></Filters><Data><Compare type='OR'>"
+	     "<Compare type='AND'><Atrybute name='CPU-frequency' compType='>=' value='1.0GHz' />"
+	     "<Atrybute name='CPU-frequency' compType='<=' value='2.0GHz' /></Compare>"
+	     "<Atrybute name='MEM-Count' compType='>=' value='4GB' /></Compare></Data></Search>");
+  
+  Search srch( str );
+
+  gc.setRequest( &srch, 666 );
   gc.getResponse( 666 );
   return 0;
 }
